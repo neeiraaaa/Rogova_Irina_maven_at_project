8b97bca545390aef8604e3f68525a11a79db058e
neeiraaaa <neeiraaaa@gmail.com>
2022-01-07 18:08:40 +0300

    Chuck Norris Parser

---
+++ src/test/java/parser/chucknoris/Parser.java
@@ -0,0 +1,11 @@
+package parser.chucknoris;
+
+import java.io.File;
+
+public class Parser {
+
+    private static final String JSON = "src/test/resources/chucknorris/chucknorris.json";
+
+    File file = new File(JSON);
+
+}
