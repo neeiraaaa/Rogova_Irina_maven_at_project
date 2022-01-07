6a0f964f28523c28e2ae58e755941ff5ed3d9be9
neeiraaaa <neeiraaaa@gmail.com>
2022-01-07 18:13:39 +0300

    constants

---
+++ src/test/java/parser/chucknoris/ApiChucknorris.java
@@ -0,0 +1,13 @@
+package parser.chucknoris;
+
+public class ApiChucknorris {
+
+    private static final String FIRST_LAST_NAME = "jokes/random?firstName=%s&lastName=%s";
+    private static final String RANDOM_JOKE = "jokes/random";
+    private static final String MULTIPLE_RANDOM_JOKE = "jokes/random/%d";
+    private static final String SPECIFIC_JOKE = "jokes/%d";
+    private static final String NUMBER_OF_JOKES = "jokes/count";
+
+    public ApiChucknorris() {
+    }
+}
